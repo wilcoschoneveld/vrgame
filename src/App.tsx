111ca49f7f6db539fe07efa3931e152a6fc4ac7b
Wilco Schoneveld <schoneveld.wj@gmail.com>
2021-04-02 21:35:21 +0200

    Moved useXR to a hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,7 @@
 import React, { useCallback, useEffect, useRef, useState } from "react";
 import { Canvas, useFrame, advance } from "@react-three/fiber";
 import { PerspectiveCamera } from "@react-three/drei";
-
-function useXR(gl?: THREE.WebGLRenderer) {
-  const [supported, setSupported] = useState(false);
-  const xr = (navigator as any).xr;
-
-  useEffect(() => {
-    const checkForXRSupport = async () => {
-      const supported = await xr.isSessionSupported("immersive-vr");
-      setSupported(supported);
-    };
-
-    xr.addEventListener("devicechange", checkForXRSupport);
-
-    checkForXRSupport();
-
-    return () => xr.removeEventListener("devicechange", checkForXRSupport);
-  }, []);
-
-  const enterXR = useCallback(async () => {
-    if (!gl) return;
-
-    const session = await xr.requestSession("immersive-vr", {
-      optionalFeatures: ["local-floor", "bounded-floor", "hand-tracking"],
-    });
-    console.log("sessions started", session);
-
-    await gl.xr.setSession(session);
-
-    // This was previously managed by @react-three/fiber vr={true}
-    gl.xr.enabled = true;
-    gl.setAnimationLoop((timestamp) => advance(timestamp, true));
-  }, [gl]);
-
-  return { supported, enterXR };
-}
+import { useXR } from "./useXR";
 
 function App() {
   const [gl, setGL] = useState<THREE.WebGLRenderer>();
---
+++ src/useXR.ts
@@ -0,0 +1,37 @@
+import { advance } from "@react-three/fiber";
+import { useCallback, useEffect, useState } from "react";
+
+export function useXR(gl?: THREE.WebGLRenderer) {
+    const [supported, setSupported] = useState(false);
+    const xr = (navigator as any).xr;
+
+    useEffect(() => {
+        const checkForXRSupport = async () => {
+            const supported = await xr.isSessionSupported("immersive-vr");
+            setSupported(supported);
+        };
+
+        xr.addEventListener("devicechange", checkForXRSupport);
+
+        checkForXRSupport();
+
+        return () => xr.removeEventListener("devicechange", checkForXRSupport);
+    }, []);
+
+    const enterXR = useCallback(async () => {
+        if (!gl) return;
+
+        const session = await xr.requestSession("immersive-vr", {
+            optionalFeatures: ["local-floor", "bounded-floor", "hand-tracking"],
+        });
+        console.log("sessions started", session);
+
+        await gl.xr.setSession(session);
+
+        // This was previously managed by @react-three/fiber vr={true}
+        gl.xr.enabled = true;
+        gl.setAnimationLoop((timestamp) => advance(timestamp, true));
+    }, [gl]);
+
+    return { supported, enterXR };
+}
