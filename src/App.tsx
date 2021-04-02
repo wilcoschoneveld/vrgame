f7ec800da05bb0ca330e06680f5eb8e7b6313379
Wilco Schoneveld <schoneveld.wj@gmail.com>
2021-04-02 20:58:56 +0200

    Working xr session

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,56 @@
-import React, { useRef, useState } from "react";
-import { Canvas, useFrame } from "@react-three/fiber";
-import { PerspectiveCamera, PointerLockControls } from "@react-three/drei";
+import React, { useCallback, useEffect, useRef, useState } from "react";
+import { Canvas, useFrame, advance } from "@react-three/fiber";
+import { PerspectiveCamera } from "@react-three/drei";
 
-function Box(props: any) {
-  // This reference will give us direct access to the mesh
-  const mesh = useRef<any>();
-  // Set up state for the hovered and active state
-  const [hovered, setHover] = useState(false);
-  const [active, setActive] = useState(false);
-  // Rotate mesh every frame, this is outside of React without overhead
-  useFrame(() => (mesh.current.rotation.x += 0.01));
+function useXR(gl?: THREE.WebGLRenderer) {
+  const [supported, setSupported] = useState(false);
+  const xr = (navigator as any).xr;
 
-  return (
-    <mesh
-      {...props}
-      ref={mesh}
-      scale={active ? 1.5 : 1}
-      onClick={(event) => setActive(!active)}
-      onPointerOver={(event) => setHover(true)}
-      onPointerOut={(event) => setHover(false)}
-    >
-      <boxGeometry args={[1, 2, 3]} />
-      <meshStandardMaterial color={hovered ? "hotpink" : "orange"} />
-    </mesh>
-  );
+  useEffect(() => {
+    const checkForXRSupport = async () => {
+      const supported = await xr.isSessionSupported("immersive-vr");
+      setSupported(supported);
+    };
+
+    xr.addEventListener("devicechange", checkForXRSupport);
+
+    checkForXRSupport();
+
+    return () => xr.removeEventListener("devicechange", checkForXRSupport);
+  }, []);
+
+  const enterXR = useCallback(async () => {
+    if (!gl) return;
+
+    const session = await xr.requestSession("immersive-vr", {
+      optionalFeatures: ["local-floor", "bounded-floor", "hand-tracking"],
+    });
+    console.log("sessions started", session);
+
+    await gl.xr.setSession(session);
+
+    // This was previously managed by @react-three/fiber vr={true}
+    gl.xr.enabled = true;
+    gl.setAnimationLoop((timestamp) => advance(timestamp, true));
+  }, [gl]);
+
+  return { supported, enterXR };
 }
 
 function App() {
+  const [gl, setGL] = useState<THREE.WebGLRenderer>();
+  const { supported, enterXR } = useXR(gl);
+
   return (
-    <Canvas>
-      <color attach="background" args={[1, 1, 1]} />
-      <PerspectiveCamera makeDefault={true} position={[0, 1.5, 0]} />
-      <gridHelper args={[100, 100]} />
-      <ambientLight />
-      <pointLight position={[10, 10, 10]} />
-      <Box position={[-1.2, 0, 0]} />
-      <Box position={[1.2, 0, 0]} />
-    </Canvas>
+    <>
+      <Canvas onCreated={({ gl }) => setGL(gl)}>
+        <color attach="background" args={[1, 1, 1]} />
+        <PerspectiveCamera makeDefault={true} position={[0, 1.5, 0]} />
+        <gridHelper args={[100, 100]} />
+        <ambientLight />
+      </Canvas>
+      {supported && <button onClick={() => enterXR()}>START VR</button>}
+    </>
   );
 }
 
