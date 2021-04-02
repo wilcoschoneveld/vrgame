a7072def9d49e9fcf5351823ee28a4ce7f260ef1
Wilco Schoneveld <schoneveld.wj@gmail.com>
2021-04-02 22:55:08 +0200

    Initial setup with eslint

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
+import eslint from '@rollup/plugin-eslint'
 import reactRefresh from '@vitejs/plugin-react-refresh'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()]
+  plugins: [
+    {
+      ...eslint({ include: 'src/**/*.+(js|jsx|ts|tsx)' }),
+      enforce: 'pre'
+    },
+    reactRefresh()]
 })
