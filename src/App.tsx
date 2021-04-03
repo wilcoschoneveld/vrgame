b09903876ecdda1731787abdbe02839d2df9371e
Wilco Schoneveld <schoneveld.wj@gmail.com>
2021-04-03 10:07:49 +0200

    Added jest and prettier

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,20 @@ import React, { useState } from "react";
 import { useXR } from "./useXR";
 
 function App() {
-  const [gl, setGL] = useState<THREE.WebGLRenderer>();
-  const { supported, enterXR } = useXR(gl);
+    const [gl, setGL] = useState<THREE.WebGLRenderer>();
+    const { supported, enterXR } = useXR(gl);
 
-  return (
-    <>
-      <Canvas onCreated={({ gl }) => setGL(gl)}>
-        <color attach="background" args={[1, 1, 1]} />
-        <PerspectiveCamera makeDefault={true} position={[0, 1.5, 0]} />
-        <gridHelper args={[100, 100]} />
-        <ambientLight />
-      </Canvas>
-      {supported && <button onClick={() => enterXR()}>START VR</button>}
-    </>
-  );
+    return (
+        <>
+            <Canvas onCreated={({ gl }) => setGL(gl)}>
+                <color attach="background" args={[1, 1, 1]} />
+                <PerspectiveCamera makeDefault={true} position={[0, 1.5, 0]} />
+                <gridHelper args={[100, 100]} />
+                <ambientLight />
+            </Canvas>
+            {supported && <button onClick={() => enterXR()}>START VR</button>}
+        </>
+    );
 }
 
 export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
-import eslint from '@rollup/plugin-eslint'
-import reactRefresh from '@vitejs/plugin-react-refresh'
-import { defineConfig } from 'vite'
+import eslint from "@rollup/plugin-eslint";
+import reactRefresh from "@vitejs/plugin-react-refresh";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    {
-      ...eslint({ include: 'src/**/*.+(ts|tsx)' }),
-      enforce: 'pre'
-    },
-    reactRefresh()]
-})
+    plugins: [
+        {
+            ...eslint({ include: "src/**/*.+(ts|tsx)" }),
+            enforce: "pre",
+        },
+        reactRefresh(),
+    ],
+});
