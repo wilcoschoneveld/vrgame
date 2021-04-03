96a47d4c7c8b2bf36fa7c5f7dc347bff75d72d55
Wilco Schoneveld <schoneveld.wj@gmail.com>
2021-04-03 10:24:51 +0200

    Added react test

---
+++ src/useXR.test.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+import { render, fireEvent, waitFor, screen } from "@testing-library/react";
+
+test("adds 1 + 2 to equal 3", () => {
+    expect(1 + 2).toBe(3);
+});
+
+test("should render react", () => {
+    render(<div>test</div>);
+});
+
+export {};
