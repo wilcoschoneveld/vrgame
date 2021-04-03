c3aa05fe86bb527ecc055bbf530a410e140a937c
Wilco Schoneveld <schoneveld.wj@gmail.com>
2021-04-03 11:36:32 +0200

    Added test for xr support

--- src/useXR.test.tsx
+++ src/useXR.test.tsx
@@ -1,4 +1,4 @@
-import { render } from "@testing-library/react";
+import { act, render } from "@testing-library/react";
 import React from "react";
 import { useXR } from "./useXR";
 
@@ -14,4 +14,61 @@ test("should not support xr", () => {
     render(<Component />);
 
     expect(supported).toBe(false);
+});
+
+test("should support xr", async () => {
+    const promise = Promise.resolve(true);
+
+    (navigator as any).xr = {
+        addEventListener: jest.fn(),
+        removeEventListener: jest.fn(),
+        isSessionSupported: jest.fn(() => promise),
+    };
+
+    let supported;
+
+    function Component() {
+        const xr = useXR();
+        supported = xr.supported;
+        return null;
+    }
+
+    render(<Component />);
+
+    await act(async () => {
+        await promise;
+    });
+
+    expect(supported).toBe(true);
+});
+
+test("should listen to devicechange events", async () => {
+    const promise = Promise.resolve(true);
+
+    const xrMock = {
+        addEventListener: jest.fn(),
+        removeEventListener: jest.fn(),
+        isSessionSupported: jest.fn(() => promise),
+    };
+
+    (navigator as any).xr = xrMock;
+
+    function Component() {
+        const xr = useXR();
+        return null;
+    }
+
+    const { unmount } = render(<Component />);
+
+    await act(async () => {
+        await promise;
+    });
+
+    expect(xrMock.addEventListener.mock.calls.length).toBe(1);
+    expect(xrMock.addEventListener.mock.calls[0][0]).toBe("devicechange");
+
+    unmount();
+
+    expect(xrMock.removeEventListener.mock.calls.length).toBe(1);
+    expect(xrMock.removeEventListener.mock.calls[0][0]).toBe("devicechange");
 });
