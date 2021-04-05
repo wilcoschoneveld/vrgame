7ea18de4e8ee2fb5341a5b3e65fa0ca3848a94cc
Wilco Schoneveld <schoneveld.wj@gmail.com>
2021-04-05 17:47:56 +0200

    Now using react-xr

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
 import { PerspectiveCamera } from "@react-three/drei";
-import { Canvas } from "@react-three/fiber";
-import React, { useState } from "react";
-import { useXR } from "./useXR";
+import { VRCanvas } from "@react-three/xr";
+import React from "react";
 
 function App() {
-    const [gl, setGL] = useState<THREE.WebGLRenderer>();
-    const { supported, enterXR } = useXR();
-
     return (
-        <>
-            <Canvas onCreated={({ gl }) => setGL(gl)}>
-                <color attach="background" args={[1, 1, 1]} />
-                <PerspectiveCamera makeDefault={true} position={[0, 1.5, 0]} />
-                <gridHelper args={[100, 100]} />
-                <ambientLight />
-            </Canvas>
-            {supported && gl && (
-                <button onClick={() => enterXR(gl)}>START VR</button>
-            )}
-        </>
+        <VRCanvas>
+            <color attach="background" args={[1, 1, 1]} />
+            <PerspectiveCamera makeDefault={true} position={[0, 1.5, 0]} />
+            <gridHelper args={[100, 100]} />
+            <ambientLight />
+        </VRCanvas>
     );
 }
 
